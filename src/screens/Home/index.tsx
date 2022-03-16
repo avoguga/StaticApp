75fc18c0073025c2c122c289c7a6e7da219f5c4c
O Gustavo <avoguga@gmail.com>
2022-03-16 15:28:07 -0300

    adding picker and adding external dts

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { View, ScrollView } from "react-native";
 import { globalStyles } from "../../styles/global";
 import { Header, SubHeader } from "../../components/Header";
 import SympleSupportedBeams from "../../components/SymplySupportedBeams";
+
 const Home = ({}) => {
-  const [beamsType, setBeamsType] = useState();
+  const [beamsType, setBeamsType] = useState("aaaaaaa");
 
   const ScreenDisplay = () => {
-    if(beamsType === "Symple Supported Beams"){
-
+    if (beamsType === "Symple Supported Beams") {
     }
     return <SympleSupportedBeams />;
   };
@@ -16,7 +16,7 @@ const Home = ({}) => {
   return (
     <View style={globalStyles.container}>
       <Header text="Beam Design Formulas" />
-      <SubHeader text="Simply Supported Beams" />
+      
       <ScrollView style={globalStyles.content}>
         <View>{ScreenDisplay()}</View>
       </ScrollView>
